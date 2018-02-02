2fe17611b8f94841b55266d8447b6fc9a082c145
hoanganhpham1006 <hoanganh.pham.12327@gmail.com>
2018-02-03 01:32:28 +0700

    Add notification_ver3

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,9 +1,21 @@
 $(document).on('turbolinks:load', function () {
   $(window).on('scroll', function(){
     more_posts_url = $('.pagination .next_page a').attr('href');
+    // if (more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - 100) {
     if (more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - 100) {
       $('.pagination').html('<img src="/assets/resources/loading.gif" alt="Loading..." title="Loading..." />');
       $.getScript(more_posts_url);
     }
   });
 });
+
+$(".dropdown-menu").on('scroll', function(){
+  console.log("a");
+  more_posts_url = $('.pagination .next_page a').attr('href');
+  // if (more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - 100) {
+  if (more_posts_url && $(window).scrollTop() > $(".dropdown-menu").height() - $("#my-notifications").height() - 100) {
+    $('.pagination').html('<img src="/assets/resources/loading.gif" alt="Loading..." title="Loading..." />');
+    $.getScript(more_posts_url);
+  }
+});
+
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,25 @@
 class Like < ApplicationRecord
-  after_create_commit {Notification.create(type_noti: 1, isSeen: false, user_set_id: self.user_id,
-    user_get_id: self.post.user_id, post_id: self.post_id)}
+  after_create_commit :create_notification
+  before_destroy :destroy_notification
 
   belongs_to :post
   belongs_to :user
   validates :user_id, presence: true
   validates :post_id, presence: true
+
+  def destroy_notification
+    n1 = Notification.where(type_noti: 1, user_set_id: self.user_id,
+    user_get_id: self.post.user_id, post_id: self.post_id).first
+    unless n1.nil?
+      n1.destroy
+    end
+  end
+
+  def create_notification
+    n1 = Notification.new(type_noti: 1, isSeen: false, user_set_id: self.user_id,
+    user_get_id: self.post.user_id, post_id: self.post_id)
+    if n1.user_set_id != n1.user_get_id
+      n1.save
+    end
+  end
 end
