f92015c2d5fc235437500ac0fd0f06641d69a82f
15thofaugust <the15thofaug@gmail.com>
2018-02-05 13:53:56 +0700

    Super mega ultra final

--- db/migrate/20180129021354_create_notifications.rb
+++ db/migrate/20180129021354_create_notifications.rb
@@ -3,9 +3,9 @@ class CreateNotifications < ActiveRecord::Migration[5.1]
     create_table :notifications do |t|
       t.integer :type_noti
       t.boolean :isSeen
-      t.references :user_set, foreign_key: true
-      t.references :user_get, foreign_key: true
-      t.references :post, foreign_key: true
+      t.integer :user_set_id, foreign_key: true
+      t.integer :user_get_id, foreign_key: true
+      t.integer :post_id, foreign_key: true
 
       t.timestamps
     end
