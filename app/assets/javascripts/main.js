8880475235600ad9f135121445e58d1e712f8286
15thofaugust <the15thofaug@gmail.com>
2018-02-05 11:38:22 +0700

    Modifying some css-es

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
-  setTimeout(function(){
-    $(".alert").fadeOut(1000);
-  },3000);
+// $(document).ready(function(){
+//   setTimeout(function(){
+//     $(".alert").fadeOut(1000);
+//   },3000);
 
     $("#post_image").bind("change", function() {
     var size_in_megabytes = this.files[0].size/1024/1024;
